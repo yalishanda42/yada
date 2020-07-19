67f845a71e1afa6cc1331ec3c38c3038f84da227
Alexander Ignatov <yalishanda@abv.bg>
2020-07-19 21:14:57 +0300

    Initial Commit

---
+++ Date Me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Date Me
+//
+//  Created by Alexander Ignatov on 19.07.20.
+//  Copyright © 2020 Alexander Ignatov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
