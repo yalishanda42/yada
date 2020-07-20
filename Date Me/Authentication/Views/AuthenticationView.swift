1b88da7e616dd6dad84eccf519af01684375ff15
Alexander Ignatov <yalishanda@abv.bg>
2020-07-20 21:54:09 +0300

    Remove ContentView

--- Date Me/Authentication/Views/AuthenticationView.swift
+++ Date Me/Authentication/Views/AuthenticationView.swift
@@ -13,70 +13,84 @@ struct AuthenticationView: View {
     @State var index = 0
         
     var body: some View {
-        VStack {
-            Image.hearts
-                .resizable()
-                .frame(width: 169, height: 169)
-                .padding(.top, 16)
-                .shadow(radius: 5)
+        ZStack {
+            LinearGradient(
+                gradient: .init(colors: [
+                    .accentOrange,
+                    .accentPeachy,
+                    .accentRed
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            ).edgesIgnoringSafeArea(.all)
             
-            HStack {
-                SegmentButton(
-                    text: "Existing",
-                    index: 0,
-                    currentIndex: $index
-                )
-                
-                SegmentButton(
-                    text: "New",
-                    index: 1,
-                    currentIndex: $index
-                )
-            }.background(Color.black.opacity(0.1))
-            .clipShape(Capsule())
-            .padding(.top, 25)
-            
-            LoginView(mode: index == 0 ? .signIn : .signUp)
-            
-            if index == 0 {
-                Button(action: {
-                }) {
-                    Text("Forgot password?".localized).foregroundColor(.white)
-                }.padding(.top, 20)
+            ScrollView(.vertical, showsIndicators: true) {
+                VStack {
+                    Image.hearts
+                        .resizable()
+                        .frame(width: 169, height: 169)
+                        .padding(.top, 16)
+                        .shadow(radius: 5)
+                    
+                    HStack {
+                        SegmentButton(
+                            text: "Existing",
+                            index: 0,
+                            currentIndex: $index
+                        )
+                        
+                        SegmentButton(
+                            text: "New",
+                            index: 1,
+                            currentIndex: $index
+                        )
+                    }.background(Color.black.opacity(0.1))
+                    .clipShape(Capsule())
+                    .padding(.top, 25)
+                    
+                    LoginView(mode: index == 0 ? .signIn : .signUp)
+                    
+                    if index == 0 {
+                        Button(action: {
+                        }) {
+                            Text("Forgot password?".localized).foregroundColor(.white)
+                        }.padding(.top, 20)
+                    }
+                    
+                    HStack(spacing: 15) {
+                        Color.white.opacity(0.7).frame(width: 35, height: 1)
+                        Text("Or".localized)
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                        Color.white.opacity(0.7).frame(width: 35, height: 1)
+                    }.padding(.top, 10)
+                    
+                    HStack {
+                        Button(action: {
+                        }) {
+                            Image.facebook
+                                .renderingMode(.original)
+                                .resizable()
+                                .frame(width: 45, height: 45)
+                                .padding()
+                        }.background(Color.white)
+                        .clipShape(Circle())
+                        
+                        Button(action: {
+                        }) {
+                            Image.apple
+                                .renderingMode(.original)
+                                .resizable()
+                                .frame(width: 45, height: 45)
+                                .padding()
+                        }.background(Color.white)
+                        .clipShape(Circle())
+                        .padding(.leading, 25)
+                    }.padding(.top, 10)
+                    
+                }.padding()
             }
-            
-            HStack(spacing: 15) {
-                Color.white.opacity(0.7).frame(width: 35, height: 1)
-                Text("Or".localized)
-                    .foregroundColor(.white)
-                    .fontWeight(.bold)
-                Color.white.opacity(0.7).frame(width: 35, height: 1)
-            }.padding(.top, 10)
-            
-            HStack {
-                Button(action: {
-                }) {
-                    Image.facebook
-                        .renderingMode(.original)
-                        .resizable()
-                        .frame(width: 45, height: 45)
-                        .padding()
-                }.background(Color.white)
-                .clipShape(Circle())
-                
-                Button(action: {
-                }) {
-                    Image.apple
-                        .renderingMode(.original)
-                        .resizable()
-                        .frame(width: 45, height: 45)
-                        .padding()
-                }.background(Color.white)
-                .clipShape(Circle())
-                .padding(.leading, 25)
-            }.padding(.top, 10)
-            
-        }.padding()
+        }
     }
 }
 
@@ -103,5 +117,11 @@ struct SegmentButton: View {
                 .frame(width: (UIScreen.main.bounds.width - 50) / 2)
         }.background(self.currentIndex == index ? Color.white : Color.clear)
         .clipShape(Capsule())
+    }
+}
+
+struct AuthenticationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticationView()
     }
 }
